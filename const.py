d2c83ba50f7960ed504547c8812ce0f93abe5498
subiawaud <subiawaud@gmail.com>
2024-10-02 12:24:08 -0600

    tidy

--- const.py
+++ const.py
@@ -5,11 +5,7 @@ load_dotenv()
 DOCKER_IMAGE = "winglian/axolotl-cloud:main-latest"
 
 CONFIG_DIR = "./configs/"
-OUTPUT_DIR = "./outputs"
-COMPLETED_MODEL_DIR = "completed-model"
 CONFIG_TEMPLATE_PATH = CONFIG_DIR + 'base.yml'
 
 HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
-
-print(f"HUGGINGFACE_TOKEN in host environment: {os.getenv('HUGGINGFACE_TOKEN')}")
 
