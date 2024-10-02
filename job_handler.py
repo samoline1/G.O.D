a4a940af2bc85d911012e152af8a5746ac94618c
subiawaud <subiawaud@gmail.com>
2024-10-02 16:56:04 -0600

    adding loss stuff

--- job_handler.py
+++ job_handler.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from schemas import Job, JobStatus, DatasetType, FileFormat
-from const import CONFIG_DIR, OUTPUT_DIR, DOCKER_IMAGE, HUGGINGFACE_TOKEN
+from const import CONFIG_DIR, DOCKER_IMAGE, HUGGINGFACE_TOKEN
 from config_handler import load_and_modify_config, save_config
 import docker
 from docker.errors import DockerException
