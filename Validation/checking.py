c641d646261675c386169c6daec4c374fa9f8ef2
subiawaud <subiawaud@gmail.com>
2024-10-02 17:22:45 -0600

    take 2 axolotl

--- Validation/checking.py
+++ Validation/checking.py
@@ -1,9 +1,10 @@
 from transformers import AutoModel, AutoConfig
-from axolotl.utils.trainer import setup_trainer
-from axolotl.utils.config import AxolotlConfig
 from schemas import TrainRequest
+from axolotl.common.cli import TrainerCliArgs
+from axolotl.cli import load_datasets
+from axolotl.train import train
+from axolotl.utils.dict import DictDefault
 from config_handler import create_dataset_entry, update_model_info
-
 import yaml
 
 def is_likely_finetune(original_repo: str, finetuned_model: AutoModel) -> bool:
@@ -16,12 +17,8 @@ def is_likely_finetune(original_repo: str, finetuned_model: AutoModel) -> bool:
     has_lora_modules = any('lora' in name.lower() for name, _ in finetuned_model.named_modules())
     return architecture_same and (base_model_match or has_peft_attributes or has_lora_modules)
 
-def evaluate_test_set_loss(config: AxolotlConfig):
-    trainer = setup_trainer(config)
-    eval_results = trainer.evaluate()
-    return eval_results
 
-def get_and_update_config(train_request: TrainRequest, config_path: str) -> AxolotlConfig:
+def get_and_update_config(train_request: TrainRequest, config_path: str) -> DictDefault:
     with open(config_path, 'r') as file:
         config_dict = yaml.safe_load(file)
 
@@ -32,10 +29,24 @@ def get_and_update_config(train_request: TrainRequest, config_path: str) -> Axol
     )
     config_dict['datasets'] = [dataset_entry]
     update_model_info(config_dict, train_request.model)
-    config = AxolotlConfig(**config_dict)
+    config = DictDefault(config_dict)
     return config
 
 def perform_evaluation(train_request: TrainRequest, config_path: str):
     config = get_and_update_config(train_request, config_path)
     eval_results = evaluate_test_set_loss(config)
     return eval_results
+
+
+def evaluate_test_set_loss(train_request: TrainRequest, config_path: str):
+    config = get_and_update_config(train_request, config_path)
+    cli_args = TrainerCliArgs()
+    dataset_meta = load_datasets(cfg=config, cli_args=cli_args)
+    model, tokenizer = train(cfg=config, cli_args=cli_args, dataset_meta=dataset_meta)
+    trainer = getattr(model, 'trainer', None)
+    
+    if trainer:
+        eval_results = trainer.evaluate()
+        return eval_results
+    else:
+        raise ValueError("Trainer not found. Unable to evaluate.")
