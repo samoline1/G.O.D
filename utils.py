9d16ea316c5b36e30f2ca706080c573444363b5d
subiawaud <subiawaud@gmail.com>
2024-10-05 21:36:45 -0600

    only console logs

--- utils.py
+++ utils.py
@@ -8,15 +8,15 @@ def setup_logger():
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setLevel(logging.INFO)
 
-    file_handler = logging.FileHandler('logging.log')
-    file_handler.setLevel(logging.INFO)
+#    file_handler = logging.FileHandler('logging.log')
+#    file_handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
+#    file_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+#    logger.addHandler(file_handler)
 
     return logger
 
