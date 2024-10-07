96eda938882653ee6d972056b52d95f7fa875240
subiawaud <subiawaud@gmail.com>
2024-10-07 16:01:34 -0600

    moving docker eval out

--- validator/evaluation/test.py
+++ validator/evaluation/test.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
         original_model="unsloth/Llama-3.2-3B-Instruct",
         dataset_type="custom",
         file_format="hf",
-        huggingface_token=os.environ["HUGGINGFACE_TOKEN"],
         system_prompt="you are helpful",
         system_format="{system}",
         field_system="text",
