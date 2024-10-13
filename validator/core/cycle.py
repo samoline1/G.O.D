ad441d6c13aa0ab738c02d41f5b635451f88a104
subiawaud <subiawaud@gmail.com>
2024-10-13 14:14:48 -0600

    starting to add in the scoring stuff

--- validator/core/cycle.py
+++ validator/core/cycle.py
@@ -4,24 +4,26 @@ from loguru import logger
 
 from validator.db import sql
 from validator.tasks.task_prep import prepare_task
+from validator.core.models import Task
 from core.models.utility_models import TaskStatus
 
-def run_task_prep(task: Task):
+def run_task_prep(task: Task) -> Task:
     output_task_repo_name = task.id
     test_data, synth_data = prepare_task(dataset_name=task.repo_name, columns_to_sample=[task.system, task.instruction, task.input_data, task.output], repo_name=output_task_repo_name)
     task.status =  TaskStatus.TRAINING
-    await update_task_info_in_the_db(task)
     return task
 
 async def validator_cycle(config):
      try:
         while True:
             logger.info("Validator Heartbeat! Its alive!")
-            tasks = await sql.get_tasks_by_status('pending', config.psql_db) # assuming we have a task object coming back
+            tasks = await sql.get_tasks_by_status(TaskStatus.PENDING, config.psql_db)
             for task in tasks:
-                task = await run_task_prep(task)
-                pass
-            logger.info(tasks)
+                task = run_task_prep(task)
+                await sql.update_task(task, config.psql_db)
+            completed_tasks = await sql.get_tasks_ready_for_evaluation(config.psql_db)
+            for completed_task in completed_tasks:
+                evaluate_and_score(completed_task, config)
             await asyncio.sleep(5)
     except asyncio.CancelledError:
         logger.info("Validator cycle cancelled, shutting down...")
--- validator/evaluation/scoring.py
+++ validator/evaluation/scoring.py
@@ -1,10 +1,71 @@
+from core.models.utility_models import CustomDatasetType, FileFormat
+from typing import List, Tuple, Dict
+from collections import defaultdict
+from validator.db.sql import get_miner_latest_submission, get_miners_assigned_to_task
 from validator.evaluation.docker_evaluation import run_evaluation_docker
+from validator.core.models import Task
+import core.constants as cts
+import numpy as np
 
-    
-# starting to sketch it out 
-#def score_miners(Task):
-    # miners = get all miners 
-#    for miner in miners: 
-        #last commit = get_last_commit(miner.id)
-        #loss, is_finetune = run_evaluation_docker(last_commit, task)
-    
+def calculate_linear_score(rank: int, total_miners: int) -> float:
+    cutoff = int(total_miners * cts.TOP_SYNTH_PERCENT_CUTOFF) # if you have a synth loss > 75% then we want to penalise you to avoid the issue of overfitting to the test set
+    if rank >= cutoff:
+        return 0
+    return 1 - 0.5 * (rank / cutoff)
+
+def calculate_score(test_results: List[Tuple[str, float]], synth_results: List[Tuple[str, float]], task: Task) -> Dict[str, float]:
+    competition_weighting = task.competition_time / cts.MAX_COMPETITION_HOURS
+    num_miners = len(test_results)
+
+    synth_ranks = {miner_id: rank for rank, (miner_id, _) in enumerate(synth_results)}
+
+    scores = {}
+    for test_rank, (miner_id, _) in enumerate(test_results):
+        test_score = calculate_linear_score(test_rank, num_miners)
+        synth_score = calculate_linear_score(synth_ranks[miner_id], num_miners)
+
+        # Penalize high loss synthetic performers, you test score will drop by half
+        if synth_ranks[miner_id] == 0:
+            test_score *= cts.PENALISATION_FACTOR_FOR_HIGH_SYNTH_LOSS
+
+        score = (cts.TEST_SCORE_WEIGHTING * test_score + (1 - cts.TEST_SCORE_WEIGHTING) * synth_score) * competition_weighting
+        scores[miner_id] = score
+
+    return scores
+
+async def evaluate_and_score(task: Task, config) -> Tuple[Dict[str, float], Dict[str, Dict[str, float]]]:
+    miner_pool = await get_miners_assigned_to_task(task.task_id, config.psql_db)
+    task_results = defaultdict(dict)
+    test_results = []
+    synth_results = []
+
+    dataset_type = CustomDatasetType(
+        field_system=task.system,
+        field_instruction=task.instruction,
+        field_input=task.input,
+        field_output=task.output
+    )
+
+    for miner in miner_pool:
+        submission_repo = get_miner_latest_submission(task.task_id, miner.id, config.psql_db)
+
+        synth_loss, is_finetune = run_evaluation_docker(dataset=task.synthetic_data,
+                                                        file_format=FileFormat.HF,
+                                                        original_model=task.model_id,
+                                                        model=submission_repo,
+                                                        dataset_type=dataset_type)
+        test_loss, _ = run_evaluation_docker(dataset=task.test_data, **evaluation_params)
+
+        task_results[miner.id] = {
+            'synth_loss': synth_loss if is_finetune else np.inf,
+            'test_loss': test_loss if is_finetune else np.inf,
+        }
+        test_results.append((miner.id, test_loss))
+        synth_results.append((miner.id, synth_loss))
+
+    ordered_test_results = sorted(test_results, key=lambda x: x[1])
+    ordered_synth_results = sorted(synth_results, key=lambda x: x[1])
+    scores = calculate_score(ordered_test_results, ordered_synth_results, task)
+
+    # now we need to update miners quality scores ...
+
