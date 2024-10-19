c6019bcc5b2d216e9fbfc6eeb056b6835ecc9802
Nick Bateman <ncbateman@gmail.com>
2024-10-19 01:06:11 +0000

    replaced ommitted sql

--- validator/db/sql.py
+++ validator/db/sql.py
@@ -339,7 +339,9 @@ async def get_tasks_ready_to_evaluate(psql_db: PSQLDB) -> List[Task]:
         connection: Connection
         rows = await connection.fetch(
             """
-
+            SELECT * FROM tasks
+            WHERE status = 'training'
+            AND NOW() AT TIME ZONE 'UTC' > end_timestamp AT TIME ZONE 'UTC'
             """
         )
         return [Task(**dict(row)) for row in rows]
