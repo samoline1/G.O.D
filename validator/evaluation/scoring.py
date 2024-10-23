aa3aaf52d5b14aa79d909e702e5781c08d4b1830
subiawaud <subiawaud@gmail.com>
2024-10-23 12:39:53 -0600

    patch

--- validator/evaluation/scoring.py
+++ validator/evaluation/scoring.py
@@ -211,6 +211,7 @@ async def evaluate_and_score(task: Task, config: Config) -> Task:
         await set_task_node_quality_score(task.task_id, result.node_id, result.score, config.psql_db)
         logger.info(f"Adding submission {result.submission}")
         if result.submission is not None:
+            result.submission.score = result.score
             await add_submission(result.submission, config.psql_db)
     logger.info(f"The final results are {task_results}")
     task.status = TaskStatus.SUCCESS
