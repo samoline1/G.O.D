faa39bd15cfd5fa3e27f0c700e90811dc9bc2828
subiawaud <subiawaud@gmail.com>
2024-10-23 18:57:39 -0600

    to open ai

--- validator/core/constants.py
+++ validator/core/constants.py
@@ -29,10 +29,12 @@ ADDITIONAL_SYNTH_DATA_PERCENTAGE = 1.0 # same size as training set
 # synth stuff
 SYNTH_GEN_BATCH_SIZE = 3
 SYNTH_MODEL_TEMPERATURE = 0.3
-SYNTH_MODEL = "llama-3-1-8b"
+#SYNTH_MODEL = "llama-3-1-8b"
+SYNTH_MODEL = "gpt-4o"
 CONTAINER_EVAL_RESULTS_PATH = "/aplp/evaluation_results.json"
-PROMPT_GEN_ENDPOINT = "https://api.corcel.io/v1/chat/completions"
-PROMPT_GEN_TOKEN = os.getenv("CORCEL_TOKEN")
+#PROMPT_GEN_ENDPOINT = "https://api.corcel.io/v1/chat/completions"
+PROMPT_GEN_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+PROMPT_GEN_TOKEN = os.getenv("OPEN_AI")
 PROMPT_PATH = "validator/prompts.yml"
 
 # Task Stuff
